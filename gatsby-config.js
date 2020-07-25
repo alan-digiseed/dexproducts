9fa46372f37774483e9a1ec1a9f70227e81754b8
Alan Wolman <alanwolman@gmail.com>
2020-07-25 12:52:49 +1000

    content updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,12 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-json`,
+      options: {
+         typeName: ({ node }) => node.name.charAt(0).toUpperCase() + node.name.slice(1)
+      }
+    },
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
