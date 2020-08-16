079d4070e9ec2f82fc778f9a3a7f043f499ed4fb
Alan Wolman <alanwolman@gmail.com>
2020-08-16 12:03:45 +1000

    Moved partname out of colors to itempart1, itempart2, itempart3; fixed bug with dimensions not being imported

--- dexdata/mappers/product_parts_mapper.js
+++ dexdata/mappers/product_parts_mapper.js
@@ -1,42 +1,62 @@
 
 const mapProductParts = (productRow) => {
        
-        let partsKeys = ["ColoursPart1", "ColoursPart2", "ColoursPart3"];
-        let numKeys = partsKeys.reduce((accumulator, key) => (productRow[key] ? ++accumulator : accumulator), 0);
-        
-        if (numKeys === 0)
-            return [];
-        else if (numKeys === 1)
-            return partsKeys.map(k => {
-                if (productRow[k] == null)
-                    return null;
-
-                let result = {
-                    partName: "",
-                    colours: productRow[k].split("|").map(i => i.trim()).filter(c => c && c !== "")
-                };
-
-                return result.colours.length > 0 ? result : null;
-            }).filter(k=>k);
-        else
+    let partsColumns = [
         {
-            return partsKeys.map(k => {
-                if (productRow[k] !== "" && productRow[k].indexOf(":")<0)
-                    throw `no partname specifed in multi-part product - Expected Format ${k} = <partname>: colour1 | colour2 ... | colour n`;
+            nameColumn: "ItemPart1",
+            colorsColumn: "ColoursPart1",
+            lengthColumn: "ItemLength1",
+            heightColumn: "ItemHeight1",
+            widthColumn: "ItemWidth1"
+        },
+        {
+            nameColumn: "ItemPart2",
+            colorsColumn: "ColoursPart2",
+            lengthColumn: "ItemLength2",
+            heightColumn: "ItemHeight2",
+            widthColumn: "ItemWidth2"
+        },
+        {
+            nameColumn: "ItemPart3",
+            colorsColumn: "ColoursPart3",
+            lengthColumn: "ItemLength3",
+            heightColumn: "ItemHeight3",
+            widthColumn: "ItemWidth3"
+        }
+    ];
+
+    let partsKeys = partsColumns.map( c => c.nameColumn);
+
+    return partsKeys.map(k => {
+
+        let columns = partsColumns.find(c => c.nameColumn === k);
+        if (!columns)
+            return null;
+
+        let partName = productRow[columns.nameColumn];
+        let colors = productRow[columns.colorsColumn];
+        let length = productRow[columns.lengthColumn];
+        let height = productRow[columns.heightColumn];
+        let width = productRow[columns.widthColumn];
+        
+        if (!partName) {
+            if (!colors && !length && !height && !width)
+                return null;
+            else
+                throw `${columns.nameColumn} requires part name. ProductCode: ${productRow["Code"]}`;
+        }
 
 
-                let partName = productRow[k].slice(0,productRow[k].indexOf(":"));
-                let colours = productRow[k].slice(productRow[k].indexOf(":") + 1);
+        let result = {
+            partName:  partName.toLowerCase() === "item" ? null : partName,
+            colours: (colors  && colors != '') ? colors.split("|").map(i => i.trim()).filter(c => c && c !== "") : new Array(),
+            length: length,
+            width: width,
+            height: height
+        };
 
-                let result = {
-                    partName: partName,
-                    colours: colours.split("|").map(i => i.trim()).filter(c => c !== "")
-                };
-
-               
-                return result.colours.length > 0 ? result : null;
-            }).filter(k=>k);
-        }            
+        return (result.partName || result.colours.length > 0) ? result : null;
+    }).filter(k=>k);
 };
 
 module.exports = mapProductParts;
--- dexdata/mappers/product_parts_mapper.test.js
+++ dexdata/mappers/product_parts_mapper.test.js
@@ -1,11 +1,31 @@
 const mapProductParts = require('./product_parts_mapper');
 
-test('single part single colours', () => {
+test('no part information', () => {
 
     productRow=Array();
+    productRow["ItemPart1"]= null;
+    productRow["ColoursPart1"]= null;
+    productRow["ColoursPart2"]=null;
+    productRow["ColoursPart3"]=null;
+    productRow["ItemPart2"]= null;
+    productRow["ItemPart3"]= null;
+
+    expect(mapProductParts(productRow)).toEqual(
+        [
+        ]
+    );
+});
+
+
+test('single part single colours - unnamed', () => {
+
+    productRow=Array();
+    productRow["ItemPart1"]= "Item";
     productRow["ColoursPart1"]= "Matt Silver";
     productRow["ColoursPart2"]=null;
     productRow["ColoursPart3"]=null;
+    productRow["ItemPart2"]= null;
+    productRow["ItemPart3"]= null;
 
     expect(mapProductParts(productRow)).toEqual(
         [
@@ -17,12 +37,63 @@ test('single part single colours', () => {
     );
 });
 
+test('single part single colours - named', () => {
+
+    productRow=Array();
+    productRow["ItemPart1"]= "Jar";
+    productRow["ColoursPart1"]= "Matt Silver";
+    productRow["ColoursPart2"]=null;
+    productRow["ColoursPart3"]=null;
+    productRow["ItemPart2"]= null;
+    productRow["ItemPart3"]= null;
+
+    expect(mapProductParts(productRow)).toEqual(
+        [
+            {
+                partName: "Jar",
+                colours: ["Matt Silver"]
+            }
+        ]
+    );
+});
+
+test('single part with dimensions only', () => {
+
+    productRow=Array();
+    productRow["ItemPart1"]= "Jar";
+    productRow["ColoursPart1"]= null;
+    productRow["ItemHeight1"] = "50";    
+    productRow["ItemLength1"] = "20";    
+    productRow["ItemWidth1"] = "10";    
+    productRow["ColoursPart2"]=null;
+    productRow["ColoursPart3"]=null;
+    productRow["ItemPart2"]= null;
+    productRow["ItemPart3"]= null;
+
+    expect(mapProductParts(productRow)).toEqual(
+        [
+            {
+                partName: "Jar",
+                colours: [],
+                length: "20",
+                height: "50",
+                width: "10"
+            }
+        ]
+    );
+});
+
+
+
 test('single part multiple colours', () => {
 
     productRow = Array();
+    productRow["ItemPart1"]= "Item";
     productRow["ColoursPart1"]= "Matt Silver | Black | White";
     productRow["ColoursPart2"]= null;
     productRow["ColoursPart3"]= null;
+    productRow["ItemPart2"]= null;
+    productRow["ItemPart3"]= null;
 
 
     expect(mapProductParts(productRow)).toEqual(
@@ -38,9 +109,12 @@ test('single part multiple colours', () => {
 test('multiple parts', () => {
 
     productRow = Array();
-    productRow["ColoursPart1"] = "Transparent Case: Matt Silver | Black | White";
-    productRow["ColoursPart2"] = "Pen: Black | Silver";
-    productRow["ColoursPart3"] = "Bow: Red | Blue";
+    productRow["ItemPart1"]= "Transparent Case";
+    productRow["ColoursPart1"] = "Matt Silver | Black | White";
+    productRow["ItemPart2"]= "Pen";
+    productRow["ColoursPart2"] = "Black | Silver";
+    productRow["ItemPart3"]= "Bow";
+    productRow["ColoursPart3"] = "Red | Blue";
 
     expect(mapProductParts(productRow)).toEqual(
         [
@@ -60,13 +134,27 @@ test('multiple parts', () => {
     );
 });
 
-test('multiple parts - no part names', () => {
+test('no part names but has colors', () => {
 
     productRow = Array();
-    productRow["ColoursPart1"] =  "Matt Silver | Black | White";
-    productRow["ColoursPart2"] =  "Pen: Black | Silver";
-    productRow["ColoursPart3"] =  "Bow: Red | Blue";
+    productRow["ItemPart1"]= null;
+    productRow["ColoursPart1"] = "Matt Silver | Black | White";
+    productRow["ItemPart2"]= "Pen";
+    productRow["ColoursPart2"] = "Black | Silver";
+    productRow["ItemPart3"]= "Bow";
+    productRow["ColoursPart3"] = "Red | Blue";
 
+    expect(() => { mapProductParts(productRow) }).toThrow();
+});
+
+test('no part names but has dimensions', () => {
+
+    productRow = Array();
+    productRow["ItemPart1"]= null;
+    productRow["ColoursPart1"] = null;
+    productRow["ItemHeight1"] = "50";    
+    productRow["ItemLength1"] = null;    
+    productRow["ItemWidth1"] = null;    
 
     expect(() => { mapProductParts(productRow) }).toThrow();
 });
