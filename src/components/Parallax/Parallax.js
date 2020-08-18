3809f6947212f66395e4be92c989a56ffa3eba0b
Alan Wolman <alanwolman@gmail.com>
2020-08-18 23:48:29 +1000

    fix for server side rendering issue

--- src/components/Parallax/Parallax.js
+++ src/components/Parallax/Parallax.js
@@ -12,25 +12,26 @@ import styles from "../../componentstyles/parallaxStyle.js";
 const useStyles = makeStyles(styles);
 
 export default function Parallax(props) {
-  let windowScrollTop;
-  if (window.innerWidth >= 768) {
-    windowScrollTop = window.pageYOffset / 3;
-  } else {
-    windowScrollTop = 0;
-  }
+  // let windowScrollTop;
+  // if (window.innerWidth >= 768) {
+  //   windowScrollTop = window.pageYOffset / 3;
+  // } else {
+  //   windowScrollTop = 0;
+  // }
+  let windowScrollTop = 0;
   const [transform, setTransform] = React.useState(
     "translate3d(0," + windowScrollTop + "px,0)"
   );
-  React.useEffect(() => {
-    if (window.innerWidth >= 768) {
-      window.addEventListener("scroll", resetTransform);
-    }
-    return function cleanup() {
-      if (window.innerWidth >= 768) {
-        window.removeEventListener("scroll", resetTransform);
-      }
-    };
-  });
+  // React.useEffect(() => {
+  //   if (window.innerWidth >= 768) {
+  //     window.addEventListener("scroll", resetTransform);
+  //   }
+  //   return function cleanup() {
+  //     if (window.innerWidth >= 768) {
+  //       window.removeEventListener("scroll", resetTransform);
+  //     }
+  //   };
+  // });
   const resetTransform = () => {
     var windowScrollTop = window.pageYOffset / 3;
     setTransform("translate3d(0," + windowScrollTop + "px,0)");
