9805a2e4460ea6828182fbf5d0ed077ea2f3a630
Alan Wolman <alanwolman@gmail.com>
2020-08-18 19:49:13 +1000

    category pages

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -1,5 +1,6 @@
 /*eslint-disable*/
 import React from "react";
+import { graphql } from 'gatsby'
 // nodejs library that concatenates classes
 import classNames from "classnames";
 // core components
